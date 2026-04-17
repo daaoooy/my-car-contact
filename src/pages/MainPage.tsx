d1c361c53c01d845ff919c9cd0bce3470eeae931
daaoooy <y020d226y@daum.net>
2026-04-17 16:28:06 +0900

    ✨ Add ContactButtons component and theme toggle functionality to MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import { cn } from "@/lib/utils";
 import CasperDongGuImage from "@/assets/casper-donggu-default.png";
+import { Button } from "@/components/shadcn/button";
+import ContactButtons from "@/components/feature/ContactButtons";
 
 const MainPage = () => {
   const [start, setStart] = useState(false);
   const [shake, setShake] = useState(false);
+  const [dark, setDark] = useState(false);
+
+  const toggleTheme = () => {
+    setDark((prev) => {
+      const next = !prev;
+      document.documentElement.classList.toggle("dark", next);
+      return next;
+    });
+  };
 
   useEffect(() => {
     if (!start) return;
@@ -25,20 +38,34 @@ const MainPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center overflow-hidden">
+    <div className="flex flex-col gap-4 h-screen items-center justify-center overflow-hidden">
+      <Button
+        onClick={toggleTheme}
+        className="absolute top-2 right-2 transition-all duration-300"
+      >
+        {dark ? <Sun /> : <Moon />}
+      </Button>
+
+      <h2>차주에게 연락이 필요하신가요?</h2>
+
+      <p className="text-sm">
+        원하는 방법을 선택해주시면 <br /> 바로 차주와 연락이 가능합니다.
+      </p>
+
       <div
-        className={`transition-all duration-7000 ease-linear
-        ${
+        className={cn(
+          "transition-all duration-7000 ease-linear",
           start
             ? "translate-x-0 translate-y-0 scale-100"
-            : "translate-x-160 -translate-y-140 scale-10"
-        }`}
+            : "translate-x-160 -translate-y-140 scale-10",
+        )}
       >
         <img
           src={CasperDongGuImage}
-          className={`w-70 h-70 scale-100 ${shake ? "translate-y-px" : ""}`}
+          className={cn("w-70 h-70 scale-100", shake ? "translate-y-px" : "")}
         />
       </div>
+      <ContactButtons />
     </div>
   );
 };
