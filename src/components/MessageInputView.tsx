4c6e4dd4e8200c1d1ac8b0b30da089f91df56d50
daaoooy <y020d226y@daum.net>
2026-04-17 21:34:26 +0900

    💄 Refactor MessageInputView layout and enhance user interaction with quick message options

--- src/components/MessageInputView.tsx
+++ src/components/MessageInputView.tsx
@@ -3,10 +3,11 @@ import { ArrowLeft, Send } from "lucide-react";
 
 import { Button } from "@/components/shadcn/button";
 import { Textarea } from "@/components/shadcn/textarea";
-import { ButtonGroup } from "@/components/shadcn/button-group";
+import { Card, CardContent, CardHeader } from "@/components/shadcn/card";
 
 import { framerSlideIn } from "@/constants/animations";
 import { QUICK_MESSAGES } from "@/constants/contacts";
+import { Separator } from "./shadcn/separator";
 
 interface MessageInputViewProps {
   message: string;
@@ -25,47 +26,70 @@ const MessageInputView = ({
     <motion.div
       key="message"
       {...framerSlideIn}
-      className="w-full flex flex-col gap-4"
+      className="w-full flex flex-col gap-5 relative"
     >
-      <div className="flex items-center text-center gap-2">
+      <div className="flex items-center justify-start gap-3 px-3">
         <Button
           variant="outline"
           onClick={onBack}
-          className="rounded-full border-gray-200 text-muted-foreground"
+          className="rounded-full border-gray-200 text-muted-foreground shrink-0"
         >
-          <ArrowLeft /> 뒤로
+          <ArrowLeft className="w-4 h-4" />
         </Button>
-        <h3>메세지로 연락하기</h3>
+
+        <div className="flex flex-col">
+          <h3>메시지로 연락하기</h3>
+          <p className="text-xs text-muted-foreground">
+            작성한 메시지는 바로 전달됩니다
+          </p>
+        </div>
       </div>
 
-      <Textarea
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="원하는 메세지를 작성하세요."
-        className="h-45 resize-none placeholder:text-sm"
-      />
+      <Card className="border-muted/50 shadow-sm">
+        <CardHeader className="pb-1">
+          <p className="text-xs text-muted-foreground">메시지를 입력하세요</p>
+        </CardHeader>
 
-      <Button
-        onClick={() => onSend()}
-        disabled={!message.trim()}
-        className="w-full gap-2 py-5 bg-[#23201e] dark:bg-[#fff9e9]"
-      >
-        <Send size={16} /> 보내기
-      </Button>
+        <CardContent>
+          <Textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="ex. 안녕하세요, 주차 때문에.."
+            className="border-none h-40 resize-none transition placeholder:text-sm"
+          />
 
-      <div className="flex flex-col gap-2 items-center mt-10">
-        <p className="text-xs text-gray-400">작성 없이 기본 메세지 보내기</p>
-        <ButtonGroup>
+          <Button
+            onClick={() => onSend()}
+            disabled={!message.trim()}
+            className="w-full gap-2 mt-4 py-5 bg-[#23201e] dark:bg-[#fff9e9]"
+          >
+            <Send className="w-4 h-4" />
+            보내기
+          </Button>
+        </CardContent>
+      </Card>
+
+      <div className="flex flex-col gap-3 mt-6">
+        <div className="flex flex-row gap-3 h-full items-center px-2">
+          <Separator className="flex-1 bg-chart-1" />
+          <p className="flex-1 text-xs text-center text-muted-foreground whitespace-nowrap">
+            또는 작성 없이 빠른 메시지 선택
+          </p>
+          <Separator className="flex-1 bg-chart-1" />
+        </div>
+
+        <div className="flex flex-wrap justify-center gap-2">
           {QUICK_MESSAGES.map((msg) => (
             <Button
-              className="bg-[#23201e] dark:bg-[#fff9e9] text-sm md:text-md"
               key={msg.label}
+              variant="outline"
               onClick={() => onSend(msg.text)}
+              className="rounded-full border-chart-2 text-xs"
             >
               {msg.label}
             </Button>
           ))}
-        </ButtonGroup>
+        </div>
       </div>
     </motion.div>
   );
